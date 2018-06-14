a3806d508c22e423a2b04f97a266e9476584deab
Fardhan Ardhi <fa.dsg.id@gmail.com>
2018-06-14 10:06:10 +0700

    Update NodeKtp

--- NodeKtp.java
+++ NodeKtp.java
@@ -1,5 +1,60 @@
 package ktpGen;
 
 public class NodeKtp {
+    String nik;
+    String nama;
+    String kotaLahir;
+    int tglLahir, blnLahir, thnLahir;
+    char jk;
+    int rt, rw;
+    String kel, kec, kota, prov;
+    String agama;
+    boolean statusNikah;
+    String pekerjaan;
+    String kwn;
     
+    NodeKtp next;
+    NodeKtp prev;
+
+    NodeKtp(
+        NodeKtp prev, 
+        String nik, 
+        String nama, 
+        String kotaLahir, 
+        int tglLahir, 
+        int blnLahir, 
+        int thnLahir, 
+        char jk,
+        int rt, 
+        int rw,
+        String kel, 
+        String kec, 
+        String kota, 
+        String prov,
+        String agama,
+        boolean statusNikah,
+        String pekerjaan,
+        String kwn,
+        NodeKtp next
+    ) {
+        this.prev = prev;
+        this.nik = nik;
+        this.nama = nama;
+        this.kotaLahir = kotaLahir;
+        this.tglLahir = tglLahir;
+        this.blnLahir = blnLahir;
+        this.thnLahir = thnLahir;
+        this.jk = jk;
+        this.rt = rt;
+        this.rw = rw;
+        this.kel = kel;
+        this.kec = kec;
+        this.kota = kota;
+        this.prov = prov;
+        this.agama = agama;
+        this.statusNikah = statusNikah;
+        this.pekerjaan = pekerjaan;
+        this.kwn = kwn;
+        this.next = next;
+    }
 }
